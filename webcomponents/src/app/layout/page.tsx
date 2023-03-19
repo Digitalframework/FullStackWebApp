d3990c4a4adb2229ffd8a5241aa1d3c6d2ea4cc6
stefan <digitalframework@outlook.de>
2023-03-19 10:08:09 +0100

    Homepage Layout needs contact page and productpage

--- webcomponents/src/app/layout/page.tsx
+++ webcomponents/src/app/layout/page.tsx
@@ -18,25 +18,33 @@ export default function Layout() {
                     <img src="https://images.pexels.com/photos/3680454/pexels-photo-3680454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
                     <img src="https://images.pexels.com/photos/13176371/pexels-photo-13176371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
                     <img src="https://images.pexels.com/photos/5089149/pexels-photo-5089149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""/>
-                    <button className={styles.refbutton}>Erfahren Sie mehr zu unseren Projekten</button>
+                    <h1 className={styles.h1carousel} style={{transform: `translateY(${height-0.2*height}%)`}}>Ein leben lang gibt es nur eine Wahl</h1>
+                        <p className={styles.pcarousel}style={{transform: `translateY(${height}%)`}}>
+                        Wir führen unsere Projekte für große Unternehmen sowie wie private Kunden sorgfaltig und schnell durch.
+                        </p>
+                    <div className={styles.buttonwrapper} style={{transform: `translateY(${height}%)`}}>
+                    <a href="#" className={styles.btn}>Unsere Projekte</a>
+                    </div>
                 </div>
                 <div className={styles.layoutcontainer}>
-                    <div className={styles.picturecontainer}>
-                        <img className={styles.layoutpicture}src="https://images.pexels.com/photos/1486470/pexels-photo-1486470.jpeg?auto=compress&cs=tinysrgb&w=1600 " alt=""/>
-                    </div>
                     <div className={styles.infotext}>
                         <h2 className={styles.h2}>Unser Anspruch auf Detail</h2>
                         <p className={styles.p}>
                         Bei ABC Carpentry sind wir stolz darauf, bei jedem Projekt, dass wir durchführen, außergewöhnliche Handwerkskunst
                         und hochwertigen Kundenservice zu bieten. Unser Team aus erfahrenen Schreinern ist auf eine breite Palette von Schreinerarbeiten
-                        spezialisiert. Mit einem Fokus zum Detail und der Verpflichtung, nur die besten Materialien zu verwenden, arbeiten wir eng mit unseren Kunden zusammen
-                        ,um ihre Visionen zum Leben zu erwecken. 
+                        spezialisiert.
 
                         </p>
                         <hr className={styles.hr}/>
                         <h3 className={styles.h3}>Treten sie mit dem Formular mit uns in Kontakt</h3>
                     </div>
                 </div>
+                <div className={styles.verticalbar} style={{transform: `translateY(${0.7*height}%)`}}></div>
+                <div className={styles.tilecontainer}> 
+                    <div className={styles.backgroundcontainer}></div>
+                    <div className={styles.productinfo}></div>
+                </div>
+                <div className={styles.contactcontainer}></div>
                 <div className={styles.formcontainer}>
                 <Form></Form>
                </div>
