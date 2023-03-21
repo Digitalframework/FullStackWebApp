bc3c86c4656c927f9dfe0a23adb488b10ef4bcf7
stefan <digitalframework@outlook.de>
2023-03-21 19:22:19 +0100

    Added minute and Date,added Scrollbar on Dropdown

--- webcomponents/src/app/form/OrderForm.tsx
+++ webcomponents/src/app/form/OrderForm.tsx
@@ -1,9 +1,12 @@
-import { useState } from 'react'
-import styles from './Form.module.css'
-import { FormWrapper } from './FormWrapper'
+import { useState } from 'react';
+import styles from './Form.module.css';
+import { FormWrapper } from './FormWrapper';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import setHours from "date-fns/setHours";
+import setMinutes from "date-fns/setMinutes";
 import { Dropdown } from './Dropdown';
+import axios from "axios";
 
 type OrderData = {
   sqm: string
@@ -22,11 +25,27 @@ export function OrderForm({
   updateFields,
 }: OrderFormProps)
  {
-  const [startDate, setStartDate] = useState(new Date());
+
+
+
+  const [startDate, setStartDate] = useState(
+    setHours(setMinutes(new Date(), 30), 16)
+  );
   return (
     <FormWrapper title="Auftragsinfos">
         <input type="text" name="sqm" placeholder="Quadratmeteranzahl" required value={sqm}/>
-        <DatePicker selected={startDate} onChange={(date:Date) => setStartDate(date)} required/>
+        <DatePicker
+      selected={startDate}
+      onChange={(date:Date) => setStartDate(date)}
+      showTimeSelect
+      excludeTimes={[
+        setHours(setMinutes(new Date(), 0), 17),
+        setHours(setMinutes(new Date(), 30), 18),
+        setHours(setMinutes(new Date(), 30), 19),
+        setHours(setMinutes(new Date(), 30), 17),
+      ]}
+      dateFormat="yyyy/MM/d/h:mm"
+    />
         <Dropdown></Dropdown>
     </FormWrapper>
     ) 
