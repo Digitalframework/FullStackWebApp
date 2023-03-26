32e236eb441269ff83bd527c7c74cec77d238ae7
stefan <digitalframework@outlook.de>
2023-03-26 17:02:06 +0200

    added changes to datepicker max date min date

--- webcomponents/src/app/form/OrderForm.tsx
+++ webcomponents/src/app/form/OrderForm.tsx
@@ -6,6 +6,7 @@ import "react-datepicker/dist/react-datepicker.css";
 import setHours from "date-fns/setHours";
 import setMinutes from "date-fns/setMinutes";
 import { Dropdown } from './Dropdown';
+import { addDays } from 'date-fns';
 import axios from "axios";
 
 type OrderData = {
@@ -28,12 +29,13 @@ export function OrderForm({
 }: OrderFormProps)
  {
   
-  const [hours, setBookedHours] = useState(Array<number>)
-  const [minutes, setBookedMinutes] = useState(Array<number>)
+
   const [bookedTimes, setBookedTimes] = useState(Array<Date>)
   const [startDate, setStartDate] = useState(
     setHours(setMinutes(new Date(), 30), 16)
   );
+  const [dateCollection, setDateCollection] = useState(Array<String>)
+
 
 
 
@@ -45,21 +47,26 @@ export function OrderForm({
     .get("http://localhost:8080/api/v1/auth/orderList")
     .then(function (response) {
       response.data.forEach(function (item:any) {
+        dateCollection.push(item.date)
         var dateInString = item.date.split("/", 4);
         console.log(dateInString)
         var time = dateInString[3].split(":", 2); 
-        console.log(time[0] + " " + time[1])
-        console.log(typeof(parseInt(time[0])))
         var bookedDates = dateInString[0] + "/" + dateInString[1] + "/" + dateInString[2]
-        console.log(bookedDates)
-        hours.push(parseInt(time[0])); 
-        minutes.push(parseInt(time[1]));
-        bookedTimes.push(setHours(setMinutes(new Date(bookedDates), parseInt(time[1])), parseInt(time[0])))
+        var currentDate = startDate.getFullYear() + "/" + (startDate.getMonth()+1)
+        + "/" + startDate.getDate()
+        console.log(bookedDates + " " + currentDate)
+        console.log(bookedDates== currentDate)
+        if(bookedDates== currentDate){
+          console.log("in if")
+          bookedTimes.push(setHours(setMinutes(new Date(bookedDates), parseInt(time[1])), parseInt(time[0])))
+          
+        }
+
     });  
     });
     }
     fetchDates()
-  }, [startDate]);
+  }, [startDate]); //?? besser anders
 
   function dateFormatter(date:Date):string{
     return date.getFullYear() + "/" + (date.getMonth()+1)
@@ -67,8 +74,9 @@ export function OrderForm({
   }
 
   function setDate(dateinp:Date) {
-    updateFields({ date: dateFormatter(dateinp) })
     setStartDate(dateinp);
+    updateFields({ date: dateFormatter(dateinp) })
+    getExcludedTimes()
     
   }
   
@@ -76,21 +84,41 @@ export function OrderForm({
     updateFields({ productType: productTypeChild })
   }
 
+  
+  function getExcludedTimes() {
+    console.log("getExcludedTimes")
+    
+    var arrExcludedTimes:Date[] = []
+    var currentDate = startDate.getFullYear() + "/" + (startDate.getMonth()+1)
+        + "/" + startDate.getDate() 
+    dateCollection.forEach(function (item:any) {
+      var dateInString = item.split("/", 4);
+      var time = dateInString[3].split(":", 2);
+      var bookedDates = dateInString[0] + "/" + dateInString[1] + "/" + dateInString[2]
+      if(bookedDates== currentDate){
+        arrExcludedTimes.push(
+          setHours(setMinutes(new Date(), parseInt(time[1])), parseInt(time[0])));
+      }
+         
+    });
+    setBookedTimes(arrExcludedTimes)
+  }
 
+  //onSelect={getExcludedTimes}        
   return (
     <FormWrapper title="Auftragsinfos">
         <input type="number" name="sqm" placeholder="Quadratmeteranzahl" required value={sqm}
       onChange={e => updateFields({ sqm: e.target.valueAsNumber })}/>
         <DatePicker
-        
+      
       selected={startDate} 
       onChange={(date:Date) => setDate(date)}
       showTimeSelect
+      minDate={new Date()}
+      maxDate={addDays(new Date(), 360)}
       minTime={setHours(setMinutes(new Date(), 0), 8)}
       maxTime={setHours(setMinutes(new Date(), 0), 19)}
-      excludeTimes={
-        bookedTimes
-      }
+      excludeTimes={bookedTimes}
       dateFormat="yyyy/MM/d/h:mm"
     />
     <div className={styles.dropdown}> 
