4047ebe848f58fd7afe29fa77d3481f302448cc6
stefan <digitalframework@outlook.de>
2023-04-29 17:58:15 +0200

    change layout and color

---
+++ webcomponents/src/app/cardcircle/page.tsx
@@ -0,0 +1,22 @@
+import styles from './Cardcircle.module.css'
+
+export default function Cardcircle() {
+
+    return (
+        <div className={styles.productcollection}>
+            <div className={styles.productimgcontainer}>
+            <img src="https://github.com/Digitalframework/FullStackWebApp/blob/main/webcomponents/src/picture/Buche.jpg?raw=true" alt="" width="300" 
+            height="300" />
+            <img src="https://github.com/Digitalframework/FullStackWebApp/blob/main/webcomponents/src/picture/Eiche.jpg?raw=true" alt="" width="300" 
+            height="300" />
+            <img src="https://github.com/Digitalframework/FullStackWebApp/blob/main/webcomponents/src/picture/Fichte.jpg?raw=true" alt="" width="300" 
+            height="300" />
+            <img src="https://github.com/Digitalframework/FullStackWebApp/blob/main/webcomponents/src/picture/Lerche.jpg?raw=true" alt="" width="300" 
+            height="300" />
+            </div>
+            <div className={styles.infotext}>
+                <p>Parkett ist ein Fußbodenbelag aus Holz oder Bambus für Innenräume.</p>
+            </div>
+        </div>
+    )
+}
